b2148a711370f1dafe3c4c97f96794b3f4bc766f
waffle <zanerisinger@gmail.com>
2025-12-21 22:00:33 -0500

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-500">
+        Study Assistant
+      </h1>
+    </main>
+  );
+}
