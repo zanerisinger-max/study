9a7c0a4eb3cdc6671f2e2b7d23e636c6afc98072
waffle <zanerisinger@gmail.com>
2025-12-24 17:52:36 -0500

    Update homepage text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-4xl font-bold text-red-500">
-        Zanes website
+      <h1 className="text-4xl font-bold text-blue-500">
+        Zanes placeholder website
       </h1>
     </main>
   );
